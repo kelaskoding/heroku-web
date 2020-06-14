947b8a831b83d6ef0050dbc73f69207cc96b436d
Hendro Steven <hendro.steven@gmail.com>
2020-06-14 23:27:11 +0700

    update title

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Heroku');
+    res.send('<h1>Hello Heroku</h1>');
 });
  
 //default Heroku PORT
